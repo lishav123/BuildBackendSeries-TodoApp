75cb87c290ee6126593a4d00e3598a3756faf158
lishav123 <aishwaraytiwary@gmail.com>
2026-03-14 22:35:58 +0530

    feat: Created the /login and /register route basic setup in main.py file and created it's structure in model.py file

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
+from model import User, CreateUser
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/register")
+def register_user(user: CreateUser):
+    return {"message": f"User registered with username {user.username}"}
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/login")
+def login_user(user: User):
+    return {"message": f"User logged in with mail {user.email}"}
---
+++ model.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    email: str
+    password: str
+
+class CreateUser(User):
+    username: str
+
+class Todo(BaseModel):
+    id: int
+    task: str
+    completed: bool
