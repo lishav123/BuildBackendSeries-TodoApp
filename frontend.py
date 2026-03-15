e9ab6b5431b447dfc87ff6aeda2d3735e5fe26f6
lishav123 <aishwaraytiwary@gmail.com>
2026-03-15 12:09:19 +0530

    created the basic flow of pages that how the stuff will work

--- frontend.py
+++ frontend.py
@@ -1,12 +1,35 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns([1,2,1])
+if "login" not in st.session_state:
+    st.session_state.login = True
+    st.session_state.authenticated = False
 
-with col2:
-    st.subheader("Login")
+if not st.session_state.authenticated:
+    st.subheader("Login" if st.session_state.login else "Register")
 
-    username = st.text_input("Username")
+    if not st.session_state.login:
+        username = st.text_input("Username")
+
+    email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
-    if st.button("Login"):
-        st.success("Logged in")
+    if st.button("Login" if st.session_state.login else "Register"):
+        st.success("Logged in" if st.session_state.login else "Registered")
+        st.session_state.authenticated = True
+        st.rerun()
+
+    if st.button("Don't have account? Register" if st.session_state.login else "Already have an account? Login"):
+        st.session_state.login = not st.session_state.login
+        st.rerun()
+
+else:
+    task = st.text_input("Enter your task")
+    button = st.button("Add the task")
+
+
+    with st.sidebar:
+        logout = st.button("Logout")
+
+    if logout:
+        st.session_state.authenticated = False
+        st.rerun()
